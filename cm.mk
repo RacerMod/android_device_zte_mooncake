zeelog/android_device_zte_blade

update N880E market fingerprint
Change-Id: I7f7320db5508b791c554a116f2257c22ddc69769

cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=blade BUILD_ID=IML74K BUILD_FINGERPRINT=ZTE/N880E_ICS/atlas40:4.0.3/IML74K/eng.zhangchun.20120426.194244:user/release-keys PRIVATE_BUILD_DESC="N880E_ICS-user 4.0.3 IML74K eng.zhangchun.20120426.194244 release-keys" BUILD_NUMBER=eng.zhangchun.20120426.194244
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=blade BUILD_ID=IML74K BUILD_FINGERPRINT=ZTE/N880E_ICS/atlas40:4.0.3/IML74K/eng..20120522.014801:user/release-keys PRIVATE_BUILD_DESC="N880E_ICS-user 4.0.3 IML74K eng..20120522.014801 release-keys" BUILD_NUMBER=eng..20120522.014801
 
 PRODUCT_NAME := cm_blade
 PRODUCT_DEVICE := blade

RacerMod/android_device_zte_mooncake

Conflicts:
	cm.mk

cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mooncake BUILD_ID=IML74K BUILD_FINGERPRINT=ZTE/N880E_ICS/atlas40:4.0.3/IML74K/eng.zhangchun.20120426.194244:user/release-keys PRIVATE_BUILD_DESC="N880E_ICS-user 4.0.3 IML74K eng.zhangchun.20120426.194244 release-keys" BUILD_NUMBER=eng.zhangchun.20120426.194244
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=mooncake BUILD_ID=IML74K BUILD_FINGERPRINT=ZTE/N880E_ICS/atlas40:4.0.3/IML74K/eng..20120522.014801:user/release-keys PRIVATE_BUILD_DESC="N880E_ICS-user 4.0.3 IML74K eng..20120522.014801 release-keys" BUILD_NUMBER=eng..20120522.014801
 
 PRODUCT_NAME := cm_mooncake
 PRODUCT_DEVICE := mooncake