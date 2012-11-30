d88eb9f01439162a0e89b3428f5b6bcf873fd77a
Michael Bestas <mikeioannina@gmail.com>
2012-11-30 07:35:51 +0200

    Fix recovery orientation & font size
    Change-Id: I2516ef7bec6c82562a7d163a5920c97adf2e2eb5

--- recovery/graphics.c
+++ recovery/graphics.c
@@ -30,7 +30,7 @@
 
 #include <pixelflinger/pixelflinger.h>
 
-#include "font_10x18.h"
+#include "font_7x16.h"
 #include "minui.h"
 
 #if defined(RECOVERY_BGRA)
@@ -176,14 +176,6 @@ void gr_flip(void)
 
     /* swap front and back buffers */
     gr_active_fb = (gr_active_fb + 1) & 1;
-
-    /* flip buffer 180 degrees for devices with physicaly inverted screens */
-    unsigned int i;
-    for (i = 1; i < (vi.xres * vi.yres); i++) {
-        unsigned short tmp = gr_mem_surface.data[i];
-        gr_mem_surface.data[i] = gr_mem_surface.data[(vi.xres * vi.yres * 2) - i];
-        gr_mem_surface.data[(vi.xres * vi.yres * 2) - i] = tmp;
-    }
 
     /* copy data from the in-memory surface to the buffer we're about
      * to make active. */
