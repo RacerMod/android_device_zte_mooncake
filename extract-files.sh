de24d7cb569e64a1cd5526fd29345b620ba1b5c8
Michael Bestas <mikeioannina@gmail.com>
2013-01-18 02:19:22 +0200

    mooncake: Update proprietary files extract method
    Change-Id: Ibe062acba57fc3f9c0be7ccf72630f290350c08e

--- extract-files.sh
+++ extract-files.sh
@@ -14,36 +14,128 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-## usage: extract-files.sh $1 $2
-## $1 and $2 are optional
-## if $1 = unzip the files will be extracted from zip file (if $1 = anything else 'adb pull' will be used
-## $2 specifies the zip file to extract from (default = ../../../${DEVICE}_update.zip)
+USAGE=\
+"script usage: extract-files.sh \$1 \$2
+\n
+\n \$1 and \$2 are optional
+\n
+\n if \$1 = unzip the files will be extracted from zip file. \$2 specifies the zip file to extract from
+\n if \$1 = usage this message is displayed
+\n if \$1 = anything else 'adb pull' will be used"
 
 VENDOR=zte
 DEVICE=mooncake
+DEVICEOUTDIR=vendor/$VENDOR/$DEVICE
+DEVICEBASE=../../../$DEVICEOUTDIR/proprietary
+DEVICEMAKEFILE=../../../$DEVICEOUTDIR/$DEVICE-vendor-blobs.mk
+DEVICEPROPS=../$DEVICE/proprietary-files.txt
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
+mkdir -p $DEVICEBASE
+rm -rf $DEVICEBASE/*
 
-if [ -z "$2" ]; then
-    ZIPFILE=../../../${DEVICE}_update.zip
+if [ "$1" = "unzip" ]; then
+    if [ -z "$2" ]; then
+        echo "You have not specified a zip file to extract from."
+        echo "Run \"extract-files.sh usage\" for more info."
+        exit
+    else
+        ZIPFILE=$2
+    fi
 else
-    ZIPFILE=$2
+    if [ "$1" = "usage" ]; then
+        echo $USAGE
+        exit
+    else
+        adb wait-for-device
+    fi
 fi
 
 if [ "$1" = "unzip" -a ! -e $ZIPFILE ]; then
-    echo $ZIPFILE does not exist.
+    echo "$ZIPFILE does not exist."
+    echo "Run \"extract-files.sh usage\" for more info."
+    exit
 else
-    for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
+    for FILE in `cat $DEVICEPROPS | grep -v ^# | grep -v ^$`; do
         DIR=`dirname $FILE`
-	if [ ! -d $BASE/$DIR ]; then
-            mkdir -p $BASE/$DIR
+	if [ ! -d $DEVICEBASE/$DIR ]; then
+            mkdir -p $DEVICEBASE/$DIR
 	fi
 	if [ "$1" = "unzip" ]; then
-            unzip -j -o $ZIPFILE system/$FILE -d $BASE/$DIR
+            echo "Unzipping mooncake specific files..."
+            unzip -j -o $ZIPFILE /$FILE -d $DEVICEBASE/$DIR
 	else
-            adb pull /system/$FILE $BASE/$FILE
+            echo "Pulling mooncake specific files..."
+            adb pull /$FILE $DEVICEBASE/$FILE
 	fi
     done
 fi
-./setup-makefiles.sh
+
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__VENDOR__/$VENDOR/g > $DEVICEMAKEFILE
+# Copyright (C) 2012-2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \\
+EOF
+
+LINEEND=" \\"
+COUNT=`cat $DEVICEPROPS | grep -v ^# | grep -v ^$ | wc -l | awk {'print $1'}`
+for FILE in `cat $DEVICEPROPS | grep -v ^# | grep -v ^$`; do
+    COUNT=`expr $COUNT - 1`
+    if [ $COUNT = "0" ]; then
+        LINEEND=""
+    fi
+    echo "    $DEVICEOUTDIR/proprietary/$FILE:$FILE$LINEEND" >> $DEVICEMAKEFILE
+done
+
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__VENDOR__/$VENDOR/g > ../../../$DEVICEOUTDIR/BoardConfigVendor.mk
+# Copyright (C) 2012-2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+EOF
+
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g | sed s/__VENDOR__/$VENDOR/g > ../../../$DEVICEOUTDIR/$DEVICE-vendor.mk
+# Copyright (C) 2012-2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Pick up overlay for features that depend on non-open-source files
+DEVICE_PACKAGE_OVERLAYS += vendor/__VENDOR__/__DEVICE__/overlay
+
+\$(call inherit-product, vendor/__VENDOR__/__DEVICE__/__DEVICE__-vendor-blobs.mk)
+EOF
+
+./../../../device/$VENDOR/mooncake-common/extract-files.sh $1 $2
